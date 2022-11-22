466bae65299ef1c776be48582ac45bf0a8e27ac3
Bayron Muñoz <b.munoz15@ufromail.cl>
2022-11-22 19:12:59 -0300

    initialize project

---
+++ src/main/java/cl/ufro/dci/kanpaiapi/KanpaiApiApplication.java
@@ -0,0 +1,13 @@
+package cl.ufro.dci.kanpaiapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KanpaiApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KanpaiApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/ufro/dci/kanpaiapi/KanpaiApiApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.ufro.dci.kanpaiapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KanpaiApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
