e0c3096b780fe805bab951d88a52bec7bd0f391a
Bayron Muñoz <b.munoz15@ufromail.cl>
2022-12-10 18:25:37 -0300

    feat: Add methods for seach mangas by name

--- src/main/java/cl/ufro/dci/kanpaiapi/service/MangaService.java
+++ src/main/java/cl/ufro/dci/kanpaiapi/service/MangaService.java
@@ -2,6 +2,7 @@ package cl.ufro.dci.kanpaiapi.service;
 
 import cl.ufro.dci.kanpaiapi.model.Manga;
 import cl.ufro.dci.kanpaiapi.repository.MangaRepository;
+import cl.ufro.dci.kanpaiapi.utils.UtilSearch;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,16 +13,23 @@ public class MangaService {
     @Autowired
     MangaRepository repository;
 
-    public Manga createManga(Manga manga){
+    public Manga createManga(Manga manga) {
         return repository.save(manga);
     }
-    public List<Manga> getAllMangas(){
+
+    public List<Manga> getAllMangas() {
         return repository.findAll();
     }
-    public Manga getMangabyID(long id){
+
+    public Manga getMangabyID(long id) {
         return repository.findById(id).orElseThrow();
     }
-    public String deleteManga(long id){
+
+    public List<Manga> searchMangabyName(String name) {
+        return repository.findAllByManNameContainingIgnoreCase(UtilSearch.formatearTexto(name));
+    }
+
+    public String deleteManga(long id) {
         Manga manga = getMangabyID(id);
         repository.delete(manga);
         return "si";
