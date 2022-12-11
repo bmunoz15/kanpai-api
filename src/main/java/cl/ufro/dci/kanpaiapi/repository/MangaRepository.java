30569827e49ae70acb70b50722e0f48be09fc407
Bayron Muñoz <b.munoz15@ufromail.cl>
2022-12-11 16:02:01 -0300

    feat: Add query to fetch by name and demography

--- src/main/java/cl/ufro/dci/kanpaiapi/repository/MangaRepository.java
+++ src/main/java/cl/ufro/dci/kanpaiapi/repository/MangaRepository.java
@@ -10,4 +10,6 @@ import java.util.Optional;
 @Repository
 public interface MangaRepository extends JpaRepository<Manga,Long> {
     List<Manga> findAllByManNameContainingIgnoreCase(String name);
+
+    List<Manga> findAllByManNameContainingIgnoreCaseAndManDemography(String name, Manga.Demography demography);
 }
