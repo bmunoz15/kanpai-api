6f2c627736470bd670bc1a578c3666f807acdbf1
Bayron Muñoz <b.munoz15@ufromail.cl>
2022-12-11 16:05:43 -0300

    refactor: delete useless methods

--- src/main/java/cl/ufro/dci/kanpaiapi/controller/MangaController.java
+++ src/main/java/cl/ufro/dci/kanpaiapi/controller/MangaController.java
@@ -9,34 +9,14 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("mangas")
+@RequestMapping("manga")
 public class MangaController {
     @Autowired
     MangaService service;
 
-    @PostMapping()
-    public ResponseEntity<Manga> createManga(@RequestBody Manga manga) {
-        return ResponseEntity.status(200).body(service.createManga(manga));
-    }
-
-    @GetMapping()
-    public ResponseEntity<List<Manga>> getAllManga() {
-        return ResponseEntity.status(200).body(service.getAllMangas());
-    }
-
     @GetMapping("/{id}")
     public ResponseEntity<Manga> getMangabyId(@PathVariable long id) {
         return ResponseEntity.status(200).body(service.getMangabyID(id));
-    }
-
-    @GetMapping("search/{name}")
-    public ResponseEntity<List<Manga>> getMangabyName(@PathVariable String name) {
-        return ResponseEntity.status(200).body(service.searchMangabyName(name));
-    }
-
-    @DeleteMapping("/{id}")
-    public ResponseEntity<String> deleteManga(@PathVariable long id) {
-        return ResponseEntity.status(200).body(service.deleteManga(id));
     }
 
 }
