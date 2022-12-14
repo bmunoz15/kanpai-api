aa876f77bb0fb7d56136ce1a8e69bd4037b7d8b7
Bayron Muñoz <b.munoz15@ufromail.cl>
2022-12-13 22:17:15 -0300

    feat: Add properties to request

--- src/main/java/cl/ufro/dci/kanpaiapi/KanpaiApiApplication.java
+++ src/main/java/cl/ufro/dci/kanpaiapi/KanpaiApiApplication.java
@@ -7,6 +7,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.Arrays;
+
 @SpringBootApplication
 public class KanpaiApiApplication {
 
@@ -20,7 +22,7 @@ public class KanpaiApiApplication {
 			private String appUrl;
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins(appUrl);
+				registry.addMapping("/**").allowedOrigins("*");
 			}
 		};
 	}
